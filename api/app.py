16967c3ed036020e2d30f106e59c0befda525135
KEWEN SILVA <kewensilva58@gmail.com>
2024-12-08 20:34:03 -0300

    [fix: muda o upload com multipart para binário]

--- api/app.py
+++ api/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'images')
 conexao = criar_conexao()
 
+
+@app.route("/teste", methods=['GET'])
+def get_teste():
+    return jsonify({"message:": "Conexão feita com sucesso"})
 # rota index, obtem todas as imagens
 @app.route('/imagens', methods=['GET'])
 def get_imagens():
@@ -17,6 +21,26 @@ def get_imagens():
 
 # rota store, resposável por inserir as imagens nos arquivos da api e inserir o caminho no banco
 @app.route('/imagens', methods=['POST'])
+def upload_imagem():
+    if request.data:
+        extensao = '.jpg'
+        filename = f"{int(datetime.now(timezone.utc).timestamp())}{extensao}"
+        caminho_imagem = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+        
+        with open(caminho_imagem, 'wb') as f:
+            f.write(request.data)
+            
+        if os.path.exists(caminho_imagem) and os.path.getsize(caminho_imagem) > 0:
+            salvar_imagem_no_banco(caminho_imagem, conexao)
+            return jsonify({"message": "Imagem criada com sucesso!"})
+        else:
+            return jsonify({"error": "Não foi possível criar a imagem!"})
+
+    
+    return jsonify({"error": "Sem dados de imagem"}), 400
+""" @app.route('/imagens', methods=['POST'])
 def upload_imagem():
     imagem = request.files.get('image')
     if imagem and imagem.filename.endswith(('jpeg', 'png', 'jpg')):
@@ -31,7 +55,7 @@ def upload_imagem():
         return jsonify({"message": "Imagem criada com sucesso!", "image_path": caminho_imagem})
     
     return jsonify({"error": "Imagem inválida ou formato não suportado!"}), 400
-
+ """
 
 # rota destroy, deleta um elemento
 @app.route('/imagens/<int:id>', methods=['DELETE'])
