0edfbc4d5f3cf098392278b51648773f543218ae
KEWEN SILVA <kewensilva58@gmail.com>
2025-01-13 10:26:41 -0300

    feat: organização do código

--- api/app.py
+++ api/app.py
@@ -1,26 +1,22 @@
-from flask import Flask, jsonify, request
-# from banco import *
-from datetime import datetime, timezone
-# from werkzeug.utils import secure_filename
+from flask import Flask
 import os
 import paho.mqtt.client as mqtt
 import time
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'images')
-# conexao = criar_conexao()
 
 # Variáveis de configuração
-endereco_broker = 'd7257de2da354fae9738d35f3212c93b.s1.eu.hivemq.cloud'
-porta = 8883
-porta_websocket = 8884
-usuario = 'mqtt_pibic'
-senha = 'Mqtt_pibic2024'
-topico_envio = 'api/send/esp'
-topico_image= "camera/image"
+address_broker = 'd7257de2da354fae9738d35f3212c93b.s1.eu.hivemq.cloud'
+port = 8883
+port_websocket = 8884
+user = 'mqtt_pibic'
+password = 'Mqtt_pibic2024'
+topic_send = 'api/send/esp'
+topic_image= "camera/image"
 
 # Variáveis globais para status
-mqtt_conectado = False
+mqtt_connected = False
 
 contador = 0
 
@@ -29,45 +25,18 @@ def on_message(client, userdata, msg):
     global contador
 
     if msg.topic == "camera/image":
-        # extensao = '.jpg'
-        # filename = f"{int(datetime.now(timezone.utc).timestamp())}{extensao}"
         image_path = os.path.join(app.config['UPLOAD_FOLDER'], f"image{contador}.jpg")
         with open(image_path, "wb") as img_file:
             img_file.write(msg.payload)
 
+        client.publish(topic_send, "1")
         # print(f"Imagem recebida via MQTT (binário) - nº {contador}")
         contador = contador+1
-        # client.publish('topico_envio', "0")
-    # contador+=1
-    # if contador>3 :
-    #     client.publish('topico_envio', "0")
-    # if ultima_mensagem["message"] == "teste":
-    #     client.publish("topico_envio", "O resultado é igual a teste")
-
-
-# # Teste com base64
-# def on_message(client, userdata, msg):
-#     global contador
-
-#     if msg.topic == "camera/image":
-#         extensao = '.jpg'
-#         filename = f"{int(datetime.now(timezone.utc).timestamp())}{extensao}"
-#         image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-#         # Decodifica a imagem base64 para binário
-#         try:
-#             image_data = base64.b64decode(msg.payload)
-#             with open(image_path, "wb") as img_file:
-#                 img_file.write(image_data)
-
-#             print(f"Imagem recebida via MQTT (base64) - nº {contador}")
-#             contador += 1
-#         except Exception as e:
-#             print(f"Erro ao decodificar imagem: {e}")
+    client.publish(topic_send, "0")
 
 def on_connect(client, userdata, flags, rc):
-    global mqtt_conectado
-    codigos_conexao = {
+    global mqtt_connected
+    cods_connection = {
         0: "Conexão bem-sucedida",
         1: "Versão do protocolo incorreta",
         2: "Identificador do cliente inválido",
@@ -77,22 +46,22 @@ def on_connect(client, userdata, flags, rc):
     }
     
     if rc == 0:
-        mqtt_conectado = True
-        print(f"{codigos_conexao[rc]}")
+        mqtt_connected = True
+        print(f"{cods_connection[rc]}")
         
         client.subscribe("camera/image")
     else:
-        mqtt_conectado = False
-        print(f"Falha na conexão: {codigos_conexao.get(rc, 'Erro desconhecido')}")
+        mqtt_connected = False
+        print(f"Falha na conexão: {cods_connection.get(rc, 'Erro desconhecido')}")
 
 def on_disconnect(client, userdata, rc):
-    global mqtt_conectado
-    mqtt_conectado = False
+    global mqtt_connected
+    mqtt_connected = False
     print("Desconectado do broker MQTT")
 
 # Configuração do cliente MQTT
 client = mqtt.Client()
-client.username_pw_set(usuario, senha)
+client.username_pw_set(user, password)
 client.tls_set()
 
 # Vincula as funções de callback
@@ -102,16 +71,16 @@ client.on_disconnect = on_disconnect
 
 # Tenta conectar ao broker
 try:
-    client.connect(endereco_broker, porta, 60)
-    client.publish(topico_envio, "mensagem 1")
+    client.connect(address_broker, port, 60)
+    client.publish(topic_send, "mensagem 1")
     client.loop_start()
     
     # Aguarda até 5 segundos pela conexão
     timeout = time.time() + 5
-    while not mqtt_conectado and time.time() < timeout:
+    while not mqtt_connected and time.time() < timeout:
         time.sleep(0.1)
     
-    if not mqtt_conectado:
+    if not mqtt_connected:
         print("Timeout na conexão MQTT")
 except Exception as e:
     print(f"Erro ao conectar ao broker MQTT: {str(e)}")
@@ -120,7 +89,7 @@ except Exception as e:
 # @app.route("/mqtt-status", methods=["GET"])
 # def get_mqtt_status():
 #     return jsonify({
-#         "connected": mqtt_conectado,
+#         "connected": mqtt_connected,
 #         "ultima_mensagem": ultima_mensagem
 #     })
 
